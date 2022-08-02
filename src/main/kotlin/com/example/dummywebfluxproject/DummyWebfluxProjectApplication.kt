e0ae73ba68c135f7c74800ea337afd57a257f5ae
Manisha Kumari <mk98.manisha@gmail.com>
2022-08-02 15:50:10 +0530

    initial commit

---
+++ src/main/kotlin/com/example/dummywebfluxproject/DummyWebfluxProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dummywebfluxproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DummyWebfluxProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<DummyWebfluxProjectApplication>(*args)
+}
